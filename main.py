b1e9f3dbb703b9cdfeedd2614f221f4c6616e1e4
ousttrue <ousttrue@gmail.com>
2021-11-16 18:27:47 +0900

    WIP main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class MyWidget(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setCentralWidget(self.create_central())
+
+        menu = self.menuBar()
+        file_menu = menu.addMenu("&File")
+        open_action = QtGui.QAction("&Open", self)
+        open_action.triggered.connect(self.open_dialog)  # type: ignore
+        file_menu.addAction(open_action)
+
+        toolbar = QtWidgets.QToolBar("My main toolbar")
+        self.addToolBar(toolbar)
+        button_action = QtGui.QAction("Click me!", self)
+        button_action.setStatusTip("rundom text")
+        button_action.triggered.connect(self.magic)  # type: ignore
+        toolbar.addAction(button_action)
+
+    def create_central(self) -> QtWidgets.QWidget:
+        layout = QtWidgets.QVBoxLayout()
+
+        self.text = QtWidgets.QLabel(
+            "Hello World", alignment=QtCore.Qt.AlignCenter)  # type: ignore
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        layout.addWidget(self.text)
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.button.clicked.connect(self.magic)  # type: ignore
+        layout.addWidget(self.button)
+
+        widget = QtWidgets.QWidget()
+        widget.setLayout(layout)
+        return widget
+
+    @QtCore.Slot()  # type: ignore
+    def open_dialog(self):
+        dialog = QtWidgets.QFileDialog(self)
+        # dialog.setFileMode(QtWidgets.QFileDialog.AnyFile)
+        dialog.setNameFilter("Images (*.png *.xpm *.jpg)")
+        if not dialog.exec():
+            return
+        fileNames = dialog.selectedFiles()
+        print(type(fileNames), fileNames)
+
+    @QtCore.Slot()  # type: ignore
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
