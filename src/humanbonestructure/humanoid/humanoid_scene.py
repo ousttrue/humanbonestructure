6db6963d89f40250a5e389f4507a471bc4e33f5b
ousttrue <ousttrue@gmail.com>
2022-04-20 19:35:00 +0900

    BoneScene

--- src/humanbonestructure/humanoid/humanoid_scene.py
+++ src/humanbonestructure/humanoid/humanoid_scene.py
@@ -9,45 +9,7 @@ from . import humanoid
 LOGGER = logging.getLogger(__name__)
 
 
-class HumanoidScene:
-    def __init__(self, root: humanoid.Bone, on_selected):
-        self.isInitialized = False
-        self.root = root
-        self.gizmo = gizmo.Gizmo()
-        self.selected: Optional[humanoid.Bone] = None
-        self.on_selected = on_selected
-
-    def _initialize(self):
-        GL.glEnable(GL.GL_DEPTH_TEST)
-        self.isInitialized = True
-
-    def _draw(self, bone: humanoid.Bone, parent: ctypesmath.Mat4):
-        matrix = bone.local_euler_matrix() * bone.local_init_matrix * parent
-
-        for i, child in enumerate(bone.children):
-            if i == 0:
-                self.gizmo.matrix = matrix
-                # self.gizmo.matrix = bone.world_matrix
-                if self.gizmo.bone(bone.bone, child.offset.get_length(), bone == self.selected):
-                    if self.selected != bone:
-                        self.selected = bone
-                        self.on_selected(self.selected)
-
-            self._draw(child, matrix)
-
-    def draw(self, state: ctypesmath.FrameState):
-        if not self.isInitialized:
-            self._initialize()
-
-        self.gizmo.begin(state)
-
-        self.gizmo.axis(10)
-        self._draw(self.root, ctypesmath.Mat4.new_identity())
-
-        self.gizmo.end()
-
-
-class SampleController:
+class BoneScene:
     def __init__(self, root, scene_selected):
         self.clear_color = (0.6, 0.6, 0.4, 0.0)
         self.camera = ctypesmath.Camera()
@@ -55,10 +17,11 @@ class SampleController:
         self.camera.view.distance = 5.0
         self.camera.view.update_matrix()
 
-        from . import humanoid_scene
-        self.scene = humanoid_scene.HumanoidScene(
-            root, scene_selected)
-        self.isInitialized = False
+        self.root = root
+        self.gizmo = gizmo.Gizmo()
+        self.selected: Optional[humanoid.Bone] = None
+        self.on_selected = scene_selected
+
         self.light = ctypesmath.Float4(1, 1, 1, 1)
 
     def onResize(self, w: int, h: int) -> bool:
@@ -103,18 +66,23 @@ class SampleController:
         '''
         milliseconds
         '''
-        if not self.scene:
-            return False
-        return self.scene.update(d)
+        return False
 
-    def initialize(self):
-        import glglue
-        LOGGER.info(glglue.get_info())
-        self.isInitialized = True
+    def _draw(self, bone: humanoid.Bone, parent: ctypesmath.Mat4):
+        matrix = bone.local_euler_matrix() * bone.local_init_matrix * parent
+
+        for i, child in enumerate(bone.children):
+            if i == 0:
+                self.gizmo.matrix = matrix
+                # self.gizmo.matrix = bone.world_matrix
+                if self.gizmo.bone(bone.bone, child.offset.get_length(), bone == self.selected):
+                    if self.selected != bone:
+                        self.selected = bone
+                        self.on_selected(self.selected)
+
+            self._draw(child, matrix)
 
     def draw(self):
-        if not self.isInitialized:
-            self.initialize()
         GL.glClearColor(*self.clear_color)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT |
                    GL.GL_DEPTH_BUFFER_BIT)  # type: ignore
@@ -123,7 +91,11 @@ class SampleController:
         GL.glViewport(int(state.viewport.x), int(state.viewport.y),
                       int(state.viewport.z), int(state.viewport.w))
 
-        if self.scene:
-            self.scene.draw(state)
+        GL.glEnable(GL.GL_DEPTH_TEST)
 
-        GL.glFlush()
+        self.gizmo.begin(state)
+
+        self.gizmo.axis(10)
+        self._draw(self.root, ctypesmath.Mat4.new_identity())
+
+        self.gizmo.end()
