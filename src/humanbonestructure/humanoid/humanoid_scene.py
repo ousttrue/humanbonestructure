decbc4bdcf24134d91f17605d3b3e0f761d86ffb
ousttrue <ousttrue@gmail.com>
2022-04-20 21:49:48 +0900

    bone_gizmo to pydear

--- src/humanbonestructure/humanoid/humanoid_scene.py
+++ src/humanbonestructure/humanoid/humanoid_scene.py
@@ -2,8 +2,9 @@ from typing import Optional
 import logging
 from OpenGL import GL
 import glm
+from pydear.scene.camera import Camera
+from pydear.scene.bone_gizmo import BoneGizmo
 from glglue import ctypesmath
-from . import bone_gizmo
 from . import humanoid
 
 
@@ -13,52 +14,78 @@ LOGGER = logging.getLogger(__name__)
 class BoneScene:
     def __init__(self, root, scene_selected):
         self.clear_color = (0.6, 0.6, 0.4, 0.0)
-        self.camera = ctypesmath.Camera()
-        self.camera.view.y = -1.0
-        self.camera.view.distance = 5.0
-        self.camera.view.update_matrix()
+        self.camera = Camera(y=-1.0, distance=5.0)
 
         self.root = root
-        self.gizmo = bone_gizmo.Gizmo()
+        self.gizmo = BoneGizmo()
         self.selected: Optional[humanoid.Bone] = None
         self.on_selected = scene_selected
 
-        self.light = ctypesmath.Float4(1, 1, 1, 1)
+        self.x = 0
+        self.y = 0
+        self.left = False
+        self.right = False
+        self.middle = False
 
     def onResize(self, w: int, h: int) -> bool:
-        return self.camera.onResize(w, h)
+        return self.camera.projection.resize(w, h)
 
     def onLeftDown(self, x: int, y: int) -> bool:
         ''' mouse input '''
-        return self.camera.onLeftDown(x, y)
+        self.left = True
+        self.x = x
+        self.y = y
+        return False
 
     def onLeftUp(self, x: int, y: int) -> bool:
         ''' mouse input '''
-        return self.camera.onLeftUp(x, y)
+        self.left = False
+        self.x = x
+        self.y = y
+        return False
 
     def onMiddleDown(self, x: int, y: int) -> bool:
         ''' mouse input '''
-        return self.camera.onMiddleDown(x, y)
+        self.middle = True
+        self.x = x
+        self.y = y
+        return False
 
     def onMiddleUp(self, x: int, y: int) -> bool:
         ''' mouse input '''
-        return self.camera.onMiddleUp(x, y)
+        self.middle = False
+        self.x = x
+        self.y = y
+        return False
 
     def onRightDown(self, x: int, y: int) -> bool:
         ''' mouse input '''
-        return self.camera.onRightDown(x, y)
+        self.right = True
+        self.x = x
+        self.y = y
+        return False
 
     def onRightUp(self, x: int, y: int) -> bool:
         ''' mouse input '''
-        return self.camera.onRightUp(x, y)
+        self.right = False
+        self.x = x
+        self.y = y
+        return False
 
     def onMotion(self, x: int, y: int) -> bool:
         ''' mouse input '''
-        return self.camera.onMotion(x, y)
+        dx = x - self.x
+        dy = y - self.y
+        self.camera.mouse_drag(x, y, dx, dy, self.left,
+                               self.right, self.middle)
+        self.x = x
+        self.y = y
+        return True
 
     def onWheel(self, d: int) -> bool:
         ''' mouse input '''
-        return self.camera.onWheel(d)
+        self.camera.wheel(-d)
+        return True
 
     def onKeyDown(self, keycode: int) -> bool:
         return False
@@ -88,23 +115,20 @@ class BoneScene:
         GL.glClear(GL.GL_COLOR_BUFFER_BIT |
                    GL.GL_DEPTH_BUFFER_BIT)  # type: ignore
 
-        state = self.camera.get_state(self.light)
-        GL.glViewport(int(state.viewport.x), int(state.viewport.y),
-                      int(state.viewport.z), int(state.viewport.w))
+        GL.glViewport(0, 0, self.camera.projection.width,
+                      self.camera.projection.height)
 
         GL.glEnable(GL.GL_DEPTH_TEST)
 
         self.gizmo.begin(
-            state.viewport,
-            state.mouse_x,
-            state.mouse_y,
-            state.mouse_left_down,
-            state.mouse_right_down,
-            state.mouse_middle_down,
-            glm.mat4(*state.camera_view),
-            glm.mat4(*state.camera_projection),
-            state.ray,
-            state.light)
+            self.x,
+            self.y,
+            self.left,
+            self.right,
+            self.middle,
+            self.camera.view.matrix,
+            self.camera.projection.matrix,
+            self.camera.get_mouse_ray(self.x, self.y))
 
         self.gizmo.axis(10)
         self._draw(self.root, ctypesmath.Mat4.new_identity())
